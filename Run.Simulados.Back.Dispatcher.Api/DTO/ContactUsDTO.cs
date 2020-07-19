4fe91ddfb9f940e2b09a5b7a8340004e47632e9f
Vitor Lassen <vitor@lassen.com.br>
2020-07-19 12:49:35 -0300

    Implement swagger

--- Run.Simulados.Back.Dispatcher.Api/DTO/ContactUsDTO.cs
+++ Run.Simulados.Back.Dispatcher.Api/DTO/ContactUsDTO.cs
@@ -9,7 +9,7 @@ namespace Run.Simulados.Back.Dispatcher.Api.DTO
     {
         public string Email { get; set; }
         public string Name { get; set; }
-        public string Lastname { get; set; }
+        public string LastName { get; set; }
         public string Phone { get; set; }
         public string Subject { get; set; }
         public string Message { get; set; }
--- Run.Simulados.Back.Dispatcher.Api/Startup.cs
+++ Run.Simulados.Back.Dispatcher.Api/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Run.Simulados.Back.Dispatcher.Api.AutoMapper;
 using Run.Simulados.Back.Dispatcher.Api.Helpers;
 
@@ -25,6 +26,15 @@ namespace Run.Simulados.Back.Dispatcher.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Run Simulados Dispatcher",
+                    Description = "Serviço para envio de emails baseado em templates ou em mensagens prontas",
+                });
+            });
             services.AddControllers();
             services.Configure<ConfigEmail>(_configuration.GetSection("ConfigEmail"));
 
@@ -40,6 +50,12 @@ namespace Run.Simulados.Back.Dispatcher.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Run Simulados Dispatcher");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
